1ad49103dd784f65c0f150335ac374b734a904f4
xeoga <bbelaci@mail.ru>
2024-05-07 08:42:03 +0300

    Pozitionarea corecta a celor 2 frame-uri si add README

--- Generator_Shell/main.py
+++ Generator_Shell/main.py
@@ -8,9 +8,10 @@ app.geometry("1100x1100")
 app.title("Shell Generate V1")
 
 
-def on_enter(event):
-    user_ip_input = ip_entry.get()
-    print(user_ip_input)
+def generate_listener_on(event):
+    user_port_input = port_entry.get()
+    print(f"nc -lvnp {user_port_input}")
+    return f'nc -lvnp {user_port_input}'
 
 
 # Create a frame 
@@ -20,19 +21,29 @@ main_frame.pack(pady=20,padx=40, fill='both',expand=True)
 main_label = cutk.CTkLabel(master=main_frame, text='Shell Generator', font=("Roboto", 32))
 main_label.pack(pady=12,padx=10)
 # Crează un frame interior în cadrul frame-ului exterior
-ip_port_frame = cutk.CTkFrame(master=main_frame, corner_radius=10, width=400, height=300)
-ip_port_frame.pack(side='left', padx=20, pady=20, fill="none", expand=False)
+ip_port_frame = cutk.CTkFrame(master=main_frame, corner_radius=10, width=400, height=400)
+ip_port_frame.pack(side='left', pady=10,padx=10, anchor='ne', fill="x", expand=False)
+
 ip_port_label = cutk.CTkLabel(master=ip_port_frame, text="IP & Port")
 ip_port_label.pack()
-ip_entry = cutk.CTkEntry(master=ip_port_frame, placeholder_text="Enter IP", width=220)
-ip_entry.pack(side="left", padx=(2, 10), pady=10)
-ip_entry.bind("<Return>", on_enter)
-ip_entry = cutk.CTkEntry(master=ip_port_frame, placeholder_text="Port", width=220)
-ip_entry.pack(side="left", padx=(2, 10), pady=10)
-ip_entry.bind("<Return>", on_enter)
 
-listent_frame = cutk.CTkFrame(master=main_frame, corner_radius=10, width=400, height=300)
-listent_frame.pack(side='right', padx=20, pady=20, fill="none", expand=False)
-#listent_label = cutk.CTkLabel(master=listent_frame, text="Listener")
-#listent_label.pack(pady=12, padx=10)
+ip_entry = cutk.CTkEntry(master=ip_port_frame, placeholder_text="Enter IP", width=200)
+ip_entry.pack(side="right", padx=(10, 20), pady=10)
+ip_entry.bind("<Return>")
+
+port_entry = cutk.CTkEntry(master=ip_port_frame, placeholder_text="Port", width=200)
+port_entry.pack(side="left", padx=(2, 10), pady=10)
+port_entry.bind("<Return>", generate_listener_on)
+
+listent_frame = cutk.CTkFrame(master=main_frame, corner_radius=10, width=400, height=40)
+listent_frame.pack(side='top', padx=20, pady=20, fill="both", expand=False)
+
+listent_label = cutk.CTkLabel(master=listent_frame, text="Listener")
+listent_label.pack(pady=12, padx=10)
+
+listent_entry = cutk.CTkEntry(master=listent_frame, placeholder_text=generate_listener_on, width=220)
+listent_entry.pack()
+# shell_frame = cutk.CTkFrame(master=main_frame, corner_radius=10)
+# shell_frame.pack(padx=20, pady=20, fill="both", expand=True)
+
 app.mainloop()
